6e3a1d51489edf3e8c0b19aa9e1b06cbeb0b0b28
Grid007 <111851589+Grid007@users.noreply.github.com>
2023-04-30 16:57:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://mgmtutility.onrender.com/graphql',
   cache,
 });
 
